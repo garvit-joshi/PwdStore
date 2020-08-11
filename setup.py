c82b9c5efb6089c203e50124ada0ce6b7c72af68
Garvit Joshi <garvitjoshi9@gmail.com>
2020-08-11 15:18:43 +0530

    First Commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PwdStore",
+    version="0.0.1",
+    author="Garvit Joshi",
+    author_email="garvitjoshi9@gmail.com",
+    description="A Package to store your Usernames, Password of a site in a file.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/garvit-joshi/PwdStore",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
