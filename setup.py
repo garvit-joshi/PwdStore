66f3d5d1e726895f43f56955573d9cacb9e66f5d
Garvit Joshi <garvitjoshi9@gmail.com>
2020-08-12 16:41:51 +0530

    now PyAescrypt will be Installed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="PwdStore",
-    version="0.1.2",
+    version="0.1.3",
     author="Garvit Joshi",
     author_email="garvitjoshi9@gmail.com",
     description="A Package to store your Usernames, Password of a site in an encrypted file.",
@@ -13,10 +13,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/garvit-joshi/PwdStore",
     packages=setuptools.find_packages(),
+    keywords = "AES Crypt encrypt decrypt password",
+    install_requires=['pyAesCrypt'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Utilities',
     ],
     python_requires='>=3.4',
 )
